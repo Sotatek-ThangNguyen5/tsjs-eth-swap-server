8287809ebe3e8cd5eba3e0c3589cd375bf4a41f3
Sotatek-ThangNguyen5 <thang.nguyen5@sotatek.com>
2021-03-09 00:05:07 +0700

    Update: Event services - update event listener

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,27 +1,66 @@
 import {WebSocketProvider} from '@ethersproject/providers';
 import {BindingScope, injectable, service} from '@loopback/core';
+import {EventFilter, utils} from 'ethers';
 import {ConnectionService} from './connection.service';
+import {TokenService} from './token.service';
 
 @injectable({scope: BindingScope.TRANSIENT})
-export class WebsocketService {
+export class Events {
+  // Private websocket Provider
   private websocketProvider: WebSocketProvider;
-  private static instance: WebsocketService;
+  // Create static instance
+  private static instance: Events;
+  // Property that describe status of websocket
+  // prevent double events on one chanel
   private isListening = false;
-
-  constructor(@service() private connectionService: ConnectionService) {
+  // Constructor for event services
+  constructor(
+    @service() private connectionService: ConnectionService,
+    @service() private tokenService: TokenService,
+  ) {
     this.websocketProvider = connectionService.getWssProvider();
   }
 
-  public static getInstance(): WebsocketService {
-    if (!WebsocketService.instance) {
+  public static getInstance(): Events {
+    if (!Events.instance) {
       // Get instance of
       const connectionService = new ConnectionService();
+      const tokenService = new TokenService(connectionService);
 
-      WebsocketService.instance = new WebsocketService(connectionService);
+      Events.instance = new Events(connectionService, tokenService);
     }
 
-    return WebsocketService.instance;
+    return Events.instance;
   }
 
-  async onNewSwapFromWxpx() {}
+  async onNewSwapFromWxpx() {
+    if (!this.isListening) {
+      const tokenContract = this.tokenService.getTokenContract();
+      const depositAddress = this.tokenService.getDepositAddress();
+      const provider = this.connectionService.getProvider();
+
+      const filter: EventFilter = {
+        address: tokenContract.address,
+        topics: [utils.id('Transfer(address,address,uint256)')],
+      };
+
+      tokenContract.on(filter, (from, to, value, transactionDetail) => {
+        console.log({from, to, value, transactionDetail});
+        // Implement a swap here
+
+        // Check if to address == deposit address
+
+        // Check value of a swap
+
+        // Validate from
+
+        // Create record for swap
+      });
+
+      console.log('RUN');
+      this.isListening = true;
+    } else {
+      console.log('Not RUN');
+    }
+  }
 }
