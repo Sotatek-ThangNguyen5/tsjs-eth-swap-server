009ef04c5fe4cc9f33b77a2a7c2c9e5c1f63db92
Sotatek-ThangNguyen5 <thang.nguyen5@sotatek.com>
2021-03-09 21:04:57 +0700

    Update: Swap Controllers

--- src/controllers/swap.controller.ts
+++ src/controllers/swap.controller.ts
@@ -1,21 +1,24 @@
 // Uncomment these imports to begin using these cool features!
 
 import {inject, service} from '@loopback/core';
+import {repository} from '@loopback/repository';
 import {get, Request, response, RestBindings} from '@loopback/rest';
+import {SwapRepository} from '../repositories';
 import {ConnectionService} from '../services';
 
 export class SwapController {
   @inject(RestBindings.Http.REQUEST) private req: Request;
   @service() private connectionService: ConnectionService;
 
-  constructor() {}
+  constructor(
+    @repository(SwapRepository) private swapRepository: SwapRepository,
+  ) {}
 
   @get('/start-swap-server')
   @response(200)
-  swap(): string {
+  async swap(): Promise<string> {
     this._startSwapServer();
-
-    return 'Server Started';
+    return 'Server started';
   }
 
   _startSwapServer() {}
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export * from './connection.service';
 export * from './token.service';
 export * from './event.service';
+export * from './swap.service';
