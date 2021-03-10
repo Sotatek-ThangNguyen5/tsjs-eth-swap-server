99ef768491522b09f79a350ba8104b16efe50290
Sotatek-ThangNguyen5 <thang.nguyen5@sotatek.com>
2021-03-10 23:16:46 +0700

    Update Swap Controller

--- src/controllers/swap.controller.ts
+++ src/controllers/swap.controller.ts
@@ -1,18 +1,20 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 // Uncomment these imports to begin using these cool features!
 
 import {inject, service} from '@loopback/core';
-import {repository} from '@loopback/repository';
 import {get, Request, response, RestBindings} from '@loopback/rest';
-import {SwapRepository} from '../repositories';
-import {ConnectionService} from '../services';
+import {ConnectionService, Events} from '../services';
 
 export class SwapController {
   @inject(RestBindings.Http.REQUEST) private req: Request;
+
   @service() private connectionService: ConnectionService;
 
-  constructor(
-    @repository(SwapRepository) private swapRepository: SwapRepository,
-  ) {}
+  private eventService: Events;
+
+  constructor() {
+    this.eventService = Events.getInstance();
+  }
 
   @get('/start-swap-server')
   @response(200)
@@ -21,5 +23,7 @@ export class SwapController {
     return 'Server started';
   }
 
-  _startSwapServer() {}
+  _startSwapServer() {
+    this.eventService.newSwap();
+  }
 }
