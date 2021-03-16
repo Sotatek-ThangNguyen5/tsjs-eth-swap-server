a1a0dbd66e52ba00a80eb9bbc229fda2a8639a22
Sotatek-ThangNguyen5 <thang.nguyen5@sotatek.com>
2021-03-16 23:28:55 +0700

    New features: ethers verify message

--- src/services/verifier.service.ts
+++ src/services/verifier.service.ts
@@ -1,10 +1,10 @@
-import {/* inject, */ BindingScope, injectable} from '@loopback/core';
-
+import {BindingScope, injectable} from '@loopback/core';
+import {Bytes, utils} from 'ethers';
 @injectable({scope: BindingScope.SINGLETON})
 export class VerifierService {
   constructor(/* Add @inject to inject parameters */) {}
 
-  /*
-   * Add service methods here
-   */
+  verifyMessage(message: Bytes, signature: any) {
+    return utils.verifyMessage(message, signature);
+  }
 }
